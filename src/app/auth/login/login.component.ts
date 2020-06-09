4c66edcf7eeef7f4af2a44cf02b8eb0cf25b7f1a
Giulietta <giulietta.atzori@moonshiner.at>
2020-06-09 18:25:49 +0200

    Create store for auth module and inject it with auth service in login component. Create login form

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {AuthService} from "../auth.service";
+import {noop} from "rxjs";
+import { tap } from 'rxjs/operators';
+import { Store } from '@ngrx/store';
+import {AppState} from '../reducers'
 
 @Component({
   selector: 'app-login',
@@ -7,9 +14,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+
+  constructor(
+    private fb:FormBuilder,
+      private auth: AuthService,
+      private router:Router,
+      private store: Store<AppState>
+  ) { 
+    this.form = fb.group({
+      email: ['test@ghiro.io', [Validators.required]],
+      password: ['test', [Validators.required]]
+    });
+  }
 
   ngOnInit(): void {
+  }
+
+  login() {
+
+    const val = this.form.value;
+
+    this.auth.login(val.email, val.password)
+      .pipe(
+        tap(user => {
+            console.log(user);
+            this.router.navigateByUrl('/');
+        })
+      )
+      .subscribe(
+        //successful login
+        noop,
+        ()=> alert('Login failed!')
+
+
+      )
+
   }
 
 }
--- src/app/auth/reducers/index.ts
+++ src/app/auth/reducers/index.ts
@@ -8,9 +8,9 @@ import {
 
 export const authFeatureKey = 'auth';
 
-export interface State {
+export interface AppState {
 }
 
-export const reducers: ActionReducerMap<State> = {
+export const reducers: ActionReducerMap<AppState> = {
 };
 
