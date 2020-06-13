6a16b137a22dd7570e6ca771c777a08a119c5d9c
Giulietta <giulietta.atzori@moonshiner.at>
2020-06-13 12:38:35 +0200

    Cretae ngrx effect for logout

--- src/app/side-menu/side-menu.component.ts
+++ src/app/side-menu/side-menu.component.ts
@@ -3,14 +3,14 @@ import { Store, select } from '@ngrx/store';
 import { AuthState } from '../auth/reducers';
 import { Observable } from 'rxjs';
 import { isLoggedIn, isLoggedOut } from '../auth/auth.selectors';
-import { logout } from '../auth/auth.actions';
+import { logout, login } from '../auth/auth.actions';
 
 @Component({
   selector: 'side-menu',
   templateUrl: './side-menu.component.html',
   styleUrls: ['./side-menu.component.css']
 })
-export class SideMenuComponent implements OnInit {
+export class SideMenuComponent {
 
   isLoggedIn$: Observable<boolean>;
   isLoggedOut$: Observable<boolean>;
@@ -18,6 +18,13 @@ export class SideMenuComponent implements OnInit {
   constructor(private store: Store<AuthState>) { }
 
   ngOnInit() {
+    const userProfile = localStorage.getItem("user");
+    if(userProfile) {
+      this.store.dispatch(login({user: JSON.parse(userProfile)}));
+      console.log(userProfile);
+    }
+    console.log('no user yet');
+
     this.isLoggedIn$ = this.store
     .pipe(
        select(isLoggedIn) 
