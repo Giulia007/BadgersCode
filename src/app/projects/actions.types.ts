8c5f3f3909fcdd18d441eefa5a7d551546c95a14
Giulietta <giulietta.atzori@moonshiner.at>
2020-06-15 11:52:10 +0200

    Create model and actions for project entity

---
+++ src/app/projects/actions.types.ts
@@ -0,0 +1,3 @@
+import * as ProjectActions from "./project.actions";
+
+export {ProjectActions};
---
+++ src/app/projects/project.actions.ts
@@ -0,0 +1,11 @@
+import { createAction, props } from "@ngrx/store" ;
+import { Project } from '../model/project';
+
+export const loadAllProjects = createAction (
+    "[Projects Resolver] Load All Projects"
+);
+
+export const allProjectsLoaded = createAction (
+    "[Load Projects Effect] All Projects Loaded",
+    props<{courses: Project[]}>()
+);
